4f23493768877e53b1f6cce67534923f7fd53bad
Tyler Holmes <pure5564@gmail.com>
2020-11-17 22:29:56 -0700

    started on volume slider

--- src/components/Player.js
+++ src/components/Player.js
@@ -5,6 +5,8 @@ import {
   faAngleLeft,
   faAngleRight,
   faPause,
+  faVolumeUp,
+  faCalculator,
 } from "@fortawesome/free-solid-svg-icons";
 
 const Player = ({
@@ -63,6 +65,20 @@ const Player = ({
     setSongInfo({ ...songInfo, currentTime: e.target.value });
   };
 
+  let volumeLevel = 0;
+  const volumeLevelHandler = (e) => {
+    audioRef.current.volume = e.target.value / 100;
+    volumeLevel = e.target.value;
+
+    setVolumeStyleStyleTransform({
+      transform: `translateX(${volumeLevel}%)`,
+      MozTransform: `translateX(${volumeLevel}%)` /* Firefox 4 */,
+      WebkitTransform: `translateX(${volumeLevel}%)` /* Safari and Chrome */,
+      OTransform: `translateX(${volumeLevel}%)` /* Opera */,
+      msTransform: `translateX(${volumeLevel}%)` /* Explorer 10 */,
+    });
+  };
+
   //pulling the index from the array of objects to know which song is next and which one was last
   // makes sure the index doesnt go bigger or smaller than whats avalible
   const skipTrackHandler = async (direction) => {
@@ -104,11 +120,29 @@ const Player = ({
     requestRef.current = requestAnimationFrame(animate);
     return () => cancelAnimationFrame(requestRef.current);
   }, [isPlaying, animationPercent]);
+
+  //this is for the animation on the volume
+  useLayoutEffect(() => {}, [isPlaying]);
+
   //sets the styke for the track
   const [styleTransform, setStyleTransform] = useState({});
+  const [volumeStyleTransform, setVolumeStyleStyleTransform] = useState({});
 
   return (
     <div className="player">
+      <div className="volume-control">
+        <div
+          style={{
+            background: `linear-gradient(to right, ${currentSong.color[0]}, ${currentSong.color[1]})`,
+          }}
+          className="volume"
+        >
+          <input min={0} max={100} type="range" onChange={volumeLevelHandler} />
+          <div style={volumeStyleTransform} className="animate-volume"></div>
+        </div>
+        <FontAwesomeIcon icon={faVolumeUp} size="2x" className="volume-icon" />
+      </div>
+
       <div className="time-control">
         <p>{getTime(songInfo.currentTime)}</p>
         <div
