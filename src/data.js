e44614b0fb1ed83b53ef231c1757870182b0e977
Tyler Holmes <pure5564@gmail.com>
2021-01-07 10:40:13 -0700

    adding github pages hosting

--- src/data.js
+++ src/data.js
@@ -1,5 +1,4 @@
 import { v4 as uuidv4 } from "uuid";
-//add songs from the Wintermix chair dance volume 11 for the new songs. Need to add lots to prevent bugs with shuffle.
 function chillHop() {
   return [
     {
